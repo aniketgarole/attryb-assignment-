f1953eae3d70db8211ed069108cf4c7e2ff2a04c
aniketgarole <aniketgarole11@gmail.com>
2023-07-19 15:33:05 +0530

    delears model added

---
+++ backend/models/dealers.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const delearsSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    
+},{
+    versionKey: false
+})
+
+
+const delearsModel = mongoose.model("delear", delearsSchema)
+
+
+module.exports = {delearsModel}
